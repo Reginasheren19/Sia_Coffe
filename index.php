72228ad068ea6d6015172870773645a148b13c1d
Reginasheren19 <112599250+Reginasheren19@users.noreply.github.com>
2024-12-23 19:02:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,26 +12,48 @@ if(isset($_SESSION['username']) && $_SESSION['role'] == 'YES'){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login Form</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-<div class="container">
-    <input type="checkbox" id="check">
-    <div class="login form">
-      <header>Login</header>
-      <form action="login.php" method="POST">
-        <input type="username" name="username" placeholder="Enter your username">
-        <input type="password" name="password" placeholder="Enter your password">
-        <input type="submit" class="button" value="Login">
-      
-    </div>
-  </div>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Login - SB Admin</title>
+        <link href="admin/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login Admin</h3></div>
+                                    <div class="card-body">
+                                        <form action="login.php" method="POST">
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputUsername" type="text" name="username" placeholder="Enter your username" />
+                                                <label for="inputUsername">Username</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputPassword" type="password" name="password" placeholder="Enter your password" />
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <div class="d-flex justify-content-center mt-4 mb-0">
+                                                <input type="submit" class="btn btn-primary" value="Login">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
 </html>
-
-
