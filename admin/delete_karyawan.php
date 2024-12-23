81ebe1a5649fb2fff9e43e549b6dfbdcfd66ae59
Reginasheren19 <112599250+Reginasheren19@users.noreply.github.com>
2024-12-24 00:12:32 +0700

    Update delete_karyawan.php

--- admin/delete_karyawan.php
+++ admin/delete_karyawan.php
@@ -1,17 +1,18 @@
 <?php
+include("../config/koneksi_mysql.php");
 
-    include("../config/koneksi_mysql.php");
+if (isset($_GET['karyawan'])) {
+    $hapus_NIK = mysqli_real_escape_string($koneksi, $_GET['karyawan']);
 
-    $hapus_nik = $_GET['nik'];
+    $sql = mysqli_query($koneksi, "DELETE FROM master_karyawan WHERE NIK = '$hapus_NIK'");
 
-    $sql = mysqli_query($koneksi,
-
-            "DELETE FROM master_karyawan WHERE NIK = '$hapus_nik'");
-
-if($sql){
-
-    header("location: master_karyawan.php");
-
+    if ($sql) {
+        header("location: master_karyawan.php");
+        exit; // Pastikan untuk menghentikan eksekusi skrip setelah header
+    } else {
+        echo "Error deleting record: " . mysqli_error($koneksi);
+    }
+} else {
+    echo "No employee specified for deletion.";
 }
-
 ?>
