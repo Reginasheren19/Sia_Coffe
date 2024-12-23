60454ef1addb0fc3bfa669ca04be761135df6724
StephanieKartika <stevinoke16@gmail.com>
2024-12-23 20:28:02 +0700

    Update master_customer.php

--- admin/master_customer.php
+++ admin/master_customer.php
@@ -179,7 +179,7 @@ error_reporting(0)
                     <div class="card mb-4">
                         <div class="card-header">
                             <i class="fas fa-table me-1"></i>
-                            Product Data Table
+                            Customer Data Table
                         </div>
                         <div class="card-body">
                             <!-- Tombol Tambah Data -->
