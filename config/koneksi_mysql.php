a51e2b744622c48b3304671029cd36c098105981
Reginasheren19 <112599250+Reginasheren19@users.noreply.github.com>
2024-12-23 17:38:49 +0700

    Update koneksi_mysql.php

--- config/koneksi_mysql.php
+++ config/koneksi_mysql.php
@@ -1,15 +1,15 @@
 <?php
+$server = "localhost";
+$user   = "root";
+$pass   = "";
+$database = "sia_coffeeshop";
 
-    $server = "localhost";
-    $user   = "root";
-    $pass   = "";
-    $database = "sia_coffeeshop";
+$koneksi = mysqli_connect($server, $user, $pass, $database);
 
-    $koneksi = mysqli_connect($server, $user, $pass);
-    mysqli_select_db($koneksi, $database);
-
-    if(!$koneksi){
-        die("Gagal connect Ke Server" . mysqli_connect_error() );
-    }
-
+// Tambahkan pemeriksaan koneksi
+if (!$koneksi) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Database connected successfully!";
+}
 ?>
