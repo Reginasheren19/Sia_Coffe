936ab66f49a19c2b73c6e2f7cfa0d57758588273
StephanieKartika <stevinoke16@gmail.com>
2024-12-24 21:45:46 +0700

    Update master_metode_pendapatan.php

--- admin/master_metode_pendapatan.php
+++ admin/master_metode_pendapatan.php
@@ -158,7 +158,7 @@ error_reporting(0)
             <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">Master Data Jenis Pendapatan</h1>
+                    <h1 class="mt-4">Master Data Metode Pembayaran</h1>
                     <ol class="breadcrumb mb-4">
                         <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
                         <li class="breadcrumb-item active">Data Jenis Pendapatan</li>
