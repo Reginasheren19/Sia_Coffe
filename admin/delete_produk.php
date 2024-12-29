8eaf780ec4c390e21534efac872c324da7036497
StephanieKartika <stevinoke16@gmail.com>
2024-12-29 14:31:39 +0700

    Update delete_produk.php

--- admin/delete_produk.php
+++ admin/delete_produk.php
@@ -6,16 +6,16 @@ echo "Parameter GET: ";
 print_r($_GET);
 echo "<br>";
 
-if (isset($_GET['produk']) && !empty($_GET['produk'])) {
+if (isset($_GET['id_produk']) && !empty($_GET['id_produk'])) { // Periksa 'id_produk'
     // Ambil ID produk dari parameter URL dan sanitasi
-    $hapus_id_produk = mysqli_real_escape_string($koneksi, $_GET['produk']);
+    $hapus_id_produk = mysqli_real_escape_string($koneksi, $_GET['id_produk']);
     echo "ID Produk yang akan dihapus: " . $hapus_id_produk . "<br>"; // Debugging
 
     // Jalankan query untuk menghapus produk berdasarkan ID
     $sql = mysqli_query($koneksi, "DELETE FROM master_produk WHERE id_produk = '$hapus_id_produk'");
 
     // Cek apakah query berhasil dieksekusi
-    if ($sql && mysqli_affected_rows($koneksi) > 0) {
+    if ($sql && mysqli_affected_rows($koneksi) > 0) { // Pastikan ada baris yang terhapus
         header("location: master_produk.php"); // Redirect ke halaman master_produk.php setelah berhasil
         exit; // Pastikan untuk menghentikan eksekusi skrip setelah header
     } else {
