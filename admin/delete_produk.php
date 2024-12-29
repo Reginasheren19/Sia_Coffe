e6d1f63e6f32318d745ee4fb78d0a801192adcbb
StephanieKartika <stevinoke16@gmail.com>
2024-12-29 14:26:14 +0700

    Update delete_produk.php

--- admin/delete_produk.php
+++ admin/delete_produk.php
@@ -2,7 +2,7 @@
 include("../config/koneksi_mysql.php");
 
 if (isset($_GET['produk'])) {
-    $hapus_id_supplier = mysqli_real_escape_string($koneksi, $_GET['produk']);
+    $hapus_id_produk = mysqli_real_escape_string($koneksi, $_GET['produk']);
     echo "ID Produk yang akan dihapus: " . $hapus_id_produk . "<br>"; // Debugging
 
     $sql = mysqli_query($koneksi, "DELETE FROM master_produk WHERE id_produk = '$hapus_id_produk'");
