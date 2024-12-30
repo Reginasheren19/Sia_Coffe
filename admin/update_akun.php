51fa207b83ba47d3846e16603a8053ca4a1601d9
Ekkle03 <esia2003mt@gmail.com>
2024-12-30 15:39:44 +0700

    sek

--- admin/update_akun.php
+++ admin/update_akun.php
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Master Akun</title>
+    <title>Master Produk</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <div class="container mt-4">
-    <h1 class="mb-4">Master Data Supplier</h1>
+    <h1 class="mb-4">Master Data Akun</h1>
 
     <!-- Tombol Tambah Data -->
     <button type="button" class="btn btn-success mb-3" data-bs-toggle="modal" data-bs-target="#addAkunModal">Add Data</button>
