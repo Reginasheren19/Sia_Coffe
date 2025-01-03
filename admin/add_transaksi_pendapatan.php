aa0361f77a1b480a3c1380c7356f09baadbd2a24
StephanieKartika <stevinoke16@gmail.com>
2025-01-03 10:54:07 +0700

    Update add_transaksi_pendapatan.php

--- admin/add_transaksi_pendapatan.php
+++ admin/add_transaksi_pendapatan.php
@@ -20,9 +20,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id_metode = mysqli_real_escape_string($koneksi, $_POST['id_metode']);
     $id_akun = mysqli_real_escape_string($koneksi, $_POST['id_akun']);
     $id_jenis_pendapatan = mysqli_real_escape_string($koneksi, $_POST['id_jenis_pendapatan']);
+    $nama_jenis_pendapatan = mysqli_real_escape_string($koneksi, $_POST['nama_jenis_pendapatan']);
     $NIK = mysqli_real_escape_string($koneksi, $_POST['NIK']);
     $nama_karyawan = mysqli_real_escape_string($koneksi, $_POST['nama_karyawan']);
     $status_transaksi = mysqli_real_escape_string($koneksi, $_POST['status_transaksi']);
+    $catatan_transaksi = mysqli_real_escape_string($koneksi, $_POST['catatan_transaksi']);
 
 
 
