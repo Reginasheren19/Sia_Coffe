c30f65b9dca0e09ecc77745493391f755582c4e2
Reginasheren19 <112599250+Reginasheren19@users.noreply.github.com>
2025-01-08 16:28:35 +0700

    aaa jurmal umum blom bisa

--- config/koneksi_mysql.php
+++ config/koneksi_mysql.php
@@ -9,7 +9,5 @@ $koneksi = mysqli_connect($server, $user, $pass, $database);
 // Tambahkan pemeriksaan koneksi
 if (!$koneksi) {
     die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Database connected successfully!";
 }
 ?>
