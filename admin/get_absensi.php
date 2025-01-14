eed3018f099091f7274f4e89559a30f5e8f8be6c
Reginasheren19 <112599250+Reginasheren19@users.noreply.github.com>
2025-01-14 22:36:28 +0700

    .

--- admin/get_absensi.php
+++ admin/get_absensi.php
@@ -42,172 +42,142 @@ $result = mysqli_query($koneksi, $query);
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Dashboard - SB Admin</title>
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-    <link href="css/styles.css" rel="stylesheet" />
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-</head>
-<body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-            <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-            </div>
-        </form>
-        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                    <li><hr class="dropdown-divider" /></li>
-                    <li><a class="dropdown-item" href="#!">Logout</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-    <div id="layoutSidenav">
-        <div id="layoutSidenav_nav">
-            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                <div class="sb-sidenav-menu">
-                    <div class="nav">
-                        <div class="sb-sidenav-menu-heading">Core</div>
-                        <a class="nav-link" href="index.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                            Dashboard
-                        </a>
-                        <div class="sb-sidenav-menu-heading">Interface</div>
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                            Layouts
-                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                        </a>
-                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                            </nav>
-                        </div>
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                            Pages
-                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                        </a>
-                        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                            <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                    Authentication
-                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                </a>
-                                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                    <nav class="sb-sidenav-menu-nested nav">
-                                        <a class="nav-link" href="login.html">Login</a>
-                                        <a class="nav-link" href="register.html">Register</a>
-                                        <a class="nav-link" href="password.html">Forgot Password</a>
-                                    </nav>
-                                </div>
-                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                    Error
-                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                </a>
-                                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                    <nav class="sb-sidenav-menu-nested nav">
-                                        <a class="nav-link" href="401.html">401 Page</a>
-                                        <a class="nav-link" href="404.html">404 Page</a>
-                                        <a class="nav-link" href="500.html">500 Page</a>
-                                    </nav>
-                                </div>
-                            </nav>
-                        </div>
-                        <div class="sb-sidenav-menu-heading">Revenue Cycle</div>
-                        <a class="nav-link" href="charts.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Pendapatan
-                        </a>
-                        <div class="sb-sidenav-menu-heading">Expenditure Cycle</div>
-                        <a class="nav-link" href="charts.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Pengeluaran
-                        </a>
-                        <div class="sb-sidenav-menu-heading">Payroll Cycle</div>
-                        <a class="nav-link" href="charts.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Kelola Karyawan
-                        </a>
-                        <a class="nav-link" href="tables.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Penggajian
-                        </a>
-                        <a class="nav-link" href="tables.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Rekap Presensi
-                        </a>
-                        <div class="sb-sidenav-menu-heading">Report Cycle</div>
-                        <a class="nav-link" href="jurnal_umum.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Jurnal Umum
-                        </a>
-                        <a class="nav-link" href="buku_besar.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Buku Besar
-                        </a>
-                        <div class="sb-sidenav-menu-heading">Mastering</div>
-                        <a class="nav-link" href="master_karyawan.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Data Karyawan
-                        </a>
-                        <a class="nav-link" href="master_jabatan.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Data Jabatan
-                        </a>
-                        <a class="nav-link" href="tables.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Data Divisi
-                        </a>
-                        <a class="nav-link" href="master_produk.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Data Produk
-                        </a>
-                        <a class="nav-link" href="master_jenis_pendapatan.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div> 
-                            Data Jenis Pendapatan
-                        </a>
-                        <a class="nav-link" href="master_metode_pembayaran.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Data Metode Pembayaran
-                        </a>
-                        <a class="nav-link" href="master_customer.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Data Customer
-                        </a>
-                        <a class="nav-link" href="master_supplier.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Data Supplier
-                        </a>
-                        <div class="sb-sidenav-menu-heading">Addons</div>
-                        <a class="nav-link" href="charts.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                            Charts
-                        </a>
-                        <a class="nav-link" href="tables.html">
-                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                            Tables
-                        </a>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SIA COFFE SHOP</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3">SIA Coffee Shop</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="../logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="dashboard_admin.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Revenue Cycle</div>
+                            <a class="nav-link" href="transaksi_pendapatan.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Pendapatan
+                            </a>
+                            <a class="nav-link" href="pelunasan_piutang.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Pelunasan Piutang
+                            </a>
+                            <a class="nav-link" href="transaksi_pendapatan_lain.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Pendapatan Lain
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Expenditure Cycle</div>
+                            <a class="nav-link" href="transaksi_pengeluaran.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Pengeluaran
+                            </a>
+                            <a class="nav-link" href="pelunasan_hutang.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Pelunasan Hutang
+                            </a>
+                            <a class="nav-link" href="transaksi_pengeluaran_lain.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Pengeluaran Lain
+                            </a>                        
+
+                            <div class="sb-sidenav-menu-heading">Payroll Cycle</div>
+                            <a class="nav-link" href="transaksi_karyawan.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Kelola Karyawan
+                            </a>
+                            <a class="nav-link" href="transaksi_penggajian.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Penggajian
+                            </a>
+                            <a class="nav-link" href="absensi.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Presensi Karyawan
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Report Cycle</div>
+                            <a class="nav-link" href="jurnal_umum.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Jurnal Umum
+                            </a>
+                            <a class="nav-link" href="buku_besar.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Buku Besar
+                            </a>
+
+                            <div class="sb-sidenav-menu-heading">Mastering</div>
+                            <a class="nav-link" href="master_karyawan.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Karyawan
+                            </a>
+                            <a class="nav-link" href="master_jabatan.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Jabatan
+                            </a>
+                            <a class="nav-link" href="master_divisi.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Divisi
+                            </a>
+                            <a class="nav-link" href="master_akun.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Akun
+                            </a>
+                            <a class="nav-link" href="master_produk.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Produk
+                            </a>
+                            <a class="nav-link" href="master_jenis_pendapatan.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Jenis Pendapatan
+                            </a>
+                            <a class="nav-link" href="master_metode_pembayaran.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Metode Pembayaran
+                            </a>
+                            <a class="nav-link" href="master_customer.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Customer
+                            </a>
+                            <a class="nav-link" href="master_supplier.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Data Supplier
+                            </a>
                     </div>
-                </div>
-                <div class="sb-sidenav-footer">
-                    <div class="small">Logged in as:</div>
-                    Start Bootstrap
-                </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
             </nav>
         </div>
         <div id="layoutSidenav_content">
