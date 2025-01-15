1eb8bb84a005743c4a9467641e2f19599ab18bc7
Reginasheren19 <112599250+Reginasheren19@users.noreply.github.com>
2025-01-15 08:30:17 +0700

    tes

--- admin/master_karyawan.php
+++ admin/master_karyawan.php
@@ -151,7 +151,7 @@ ini_set('display_errors', 1);
                 <div class="container-fluid px-4">
                     <h1 class="mt-4">Master Data Karyawan</h1>
                     <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item"><a href="dashboard_admin.php">Dashboard</a></li>
                         <li class="breadcrumb-item active">Data Karyawan</li>
                     </ol>
                     <div class="card mb-4">
